b929b7129f0436eaa18057be1f7e8c1c2a057c2c
Bruno Unna <bunna@xanadu.ie>
2016-09-27 15:31:14 +0100

    successfully using redis as a cache mechanism

--- proxy-service/src/main/java/com/fexco/test/ProxyService.java
+++ proxy-service/src/main/java/com/fexco/test/ProxyService.java
@@ -1,11 +1,9 @@
 package com.fexco.test;
 
-import io.netty.handler.codec.http.HttpHeaderValues;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.json.Json;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
@@ -13,6 +11,12 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.redis.RedisClient;
 import io.vertx.redis.RedisOptions;
+
+import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON;
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
+import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 /**
  * A verticle delivering real responses for eircode queries.
@@ -23,6 +27,7 @@ public class ProxyService extends AbstractVerticle {
 
     private Logger logger = LoggerFactory.getLogger(ProxyService.class);
     private RedisClient redis;
+    private HttpClient externalHttpClient;
 
     @Override
     public void start(Future<Void> future) throws Exception {
@@ -63,6 +68,9 @@ public class ProxyService extends AbstractVerticle {
             }
         });
 
+        // a http client for the external service will be needed too
+        externalHttpClient = vertx.createHttpClient();
+
         // create the routes that are recognised by the service
         // and send requests to appropriate handler/catalog
         Router router = Router.router(vertx);
@@ -93,47 +101,78 @@ public class ProxyService extends AbstractVerticle {
      *
      * @param routingContext routing context as provided by vertx-web
      */
-    protected void handleRequest(RoutingContext routingContext, AddressCatalog catalog) {
+    private void handleRequest(RoutingContext routingContext, AddressCatalog catalog) {
         String apiKey = routingContext.request().getParam("apiKey");
         String fragment = routingContext.request().getParam("fragment");
         String format = routingContext.request().getParam("format");
         logger.info("received a request, apiKey=" + apiKey + ", fragment=" + fragment);
 
-        routingContext.response().putHeader(HttpHeaders.CONTENT_TYPE.toString(), HttpHeaderValues.APPLICATION_JSON);
+        routingContext.response().putHeader(CONTENT_TYPE.toString(), APPLICATION_JSON);
 
         if (apiKey == null || fragment == null) {
             routingContext
                     .response()
-                    .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+                    .setStatusCode(BAD_REQUEST.code())
                     .end(new JsonObject()
-                            .put("code", HttpResponseStatus.BAD_REQUEST.code())
-                            .put("message", HttpResponseStatus.BAD_REQUEST.reasonPhrase())
+                            .put("code", BAD_REQUEST.code())
+                            .put("message", BAD_REQUEST.reasonPhrase())
                             .encodePrettily());
         } else {
-            routingContext
-                    .response()
-                    // TODO make this behaviour richer: can't be a 200 always!!!
-                    .setStatusCode(HttpResponseStatus.OK.code())
-                    .end(Json.encodePrettily(getAddressesString(apiKey, catalog, fragment)));
+            sendResponseTo(routingContext.response(), apiKey, catalog, fragment);
         }
     }
 
     /**
-     * Looks for the required fragment in redis and returns it if found; otherwise
-     * queries the proxy-ed service, stores the result in redis and returns it.
+     * Look for the required fragment in redis. If not found, query the proxy-ed service and store
+     * the result in redis. In any case, send the response through the given response object.
      *
+     * @param response response object to use when sending the response
      * @param apiKey   authentication token from the client
      * @param catalog  catalog upon which the query is to be performed
      * @param fragment string for which the search is performed  @return result of the search,
      *                 either fresh or cached
      */
-    protected String getAddressesString(String apiKey, AddressCatalog catalog, String fragment) {
+    private void sendResponseTo(HttpServerResponse response, String apiKey, AddressCatalog catalog, String fragment) {
         redis.get(catalog.getPrefix() + ":" + fragment, stringAsyncResult -> {
             if (stringAsyncResult.failed()) {
-
+                response
+                        .setStatusCode(INTERNAL_SERVER_ERROR.code())
+                        .end(new JsonObject()
+                                .put("code", INTERNAL_SERVER_ERROR.code())
+                                .put("message", INTERNAL_SERVER_ERROR.reasonPhrase())
+                                .encodePrettily());
+            } else {
+                String redisResult = stringAsyncResult.result();
+                if (redisResult != null) {
+                    // found it in redis, just return it
+                    logger.info("Fragment [" + fragment + "] has been found in redis, returning it");
+                    response
+                            .setStatusCode(OK.code())
+                            .end(redisResult);
+                } else {
+                    logger.info("Fragment [" + fragment + "] has NOT been found in redis, querying it");
+                    externalHttpClient.getNow(80, "ws.postcoder.com", "/pcw/PCW45-12345-12345-1234X/address/ie/D02X285?format=json",
+                            httpClientResponse -> {
+                                logger.info("Reply from postcoder has been received");
+                                if (httpClientResponse.statusCode() != OK.code()) {
+                                    logger.info("But reply from postcoder is not ok");
+                                    response.setStatusCode(INTERNAL_SERVER_ERROR.code())
+                                            .end(httpClientResponse.statusMessage());
+                                } else {
+                                    logger.info("And reply from postcoder is ok");
+                                    httpClientResponse.bodyHandler(buffer -> {
+                                        redis.set(catalog.getPrefix() + ":" + fragment, buffer.toString(), voidAsyncResult -> {
+                                            logger.info("Stored fragment [" + fragment + "] in redis, with value [" + buffer.toString() + "]");
+                                        });
+                                        response.setStatusCode(OK.code())
+                                                .end(buffer);
+                                    });
+                                }
+                            });
+                }
             }
         });
-        return null;
+
     }
 
 }
